327b8175943b61239932d10c0da242b036115962
HungVu <vuhung.vtq@gmail.com>
2024-07-30 23:57:15 +0700

    first commit

---
+++ build/web/error.jsp
@@ -0,0 +1,12 @@
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Error</title>
+</head>
+<body>
+    <h1>Error</h1>
+    <p>${errorMessage}</p>
+</body>
+</html>
---
+++ src/java/control/HomeControl.java
@@ -0,0 +1,48 @@
+package control;
+
+import dao.DAO;
+import entity.Category;
+import entity.Product;
+import java.io.IOException;
+import java.util.List;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class HomeControl extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+
+        // b1: get data from DAO
+        DAO dao = new DAO();
+        List<Product> list = dao.getAllProduct();
+        List<Category> listC = dao.getAllCategory();
+        Product last = dao.getLast();
+        
+        //b2: set data to jsp
+        request.setAttribute("listP", list);
+        request.setAttribute("listCC", listC);
+        request.setAttribute("p", last);
+        request.getRequestDispatcher("Home.jsp").forward(request, response);
+    } 
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        processRequest(request, response);
+    } 
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
+}
